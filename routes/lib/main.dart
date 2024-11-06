ff7daa2af8558036302609a10547d0bf12fca25c
Rosimaltasari <167852703+Rosimaltasari@users.noreply.github.com>
2024-11-06 09:23:08 +0700

    Update main.dart

--- routes/lib/main.dart
+++ routes/lib/main.dart
@@ -10,7 +10,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainApp(),
+      home: HomePage(),
       routes: {
         '/homePage': (context) => HomePage(),
         '/gallerypage': (context) => GalleryPage(),
